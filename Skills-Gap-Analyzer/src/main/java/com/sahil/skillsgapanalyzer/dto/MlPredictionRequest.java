67221f31da59fe873bbbb2c703ffa53ec907b0f9
victo-silvers <silversvicto@gmail.com>
2026-03-30 00:30:26 +0530

    defined attributes for ML prediction request

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/MlPredictionRequest.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/MlPredictionRequest.java
@@ -1,4 +1,13 @@
 package com.sahil.skillsgapanalyzer.dto;
 
+
+//What springboot sends to Flask
 public class MlPredictionRequest {
+    public Integer skills_attempt;
+    public Double skill_accuracy;
+    public Integer total_attempts;
+    public Double overall_Accuracy;
+    public Double recent_accuracy;
+    public Integer hintCount;
+    public Double timeTaken;
 }
