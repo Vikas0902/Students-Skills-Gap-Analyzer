22f309f6e47667493aaadce1155f145fca021805
victo-silvers <silversvicto@gmail.com>
2026-03-29 15:07:23 +0530

    updated SkillPredictionDto

--- Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/SkillPredictionDto.java
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/dto/SkillPredictionDto.java
@@ -1,5 +1,20 @@
 package com.sahil.skillsgapanalyzer.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SkillPredictionDto {
-    
+    private String skill;
+    private Double masteryScore;
+    private String status;
+    private String trend;
+    private Double avgTime;
+    private Integer totalHints;
+    private Double Confidence;
 }
