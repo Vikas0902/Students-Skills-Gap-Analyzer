d7db51581ae8ab6fbc5ccebfcb665fa04091cbc4
victo-silvers <silversvicto@gmail.com>
2026-03-30 04:10:38 +0530

    creating endpoint for dashboard

---
+++ Skills-Gap-Analyzer/src/main/java/com/sahil/skillsgapanalyzer/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package com.sahil.skillsgapanalyzer.controller;
+
+
+import com.sahil.skillsgapanalyzer.dto.DashboardDataResponse;
+import com.sahil.skillsgapanalyzer.service.DashboardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.lang.model.util.Elements;
+
+ // Allows this api to be called with CORS errors
+public class DashboardController {
+
+}
